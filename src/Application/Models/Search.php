cd801c9c176c0d6028dcd949fe18fd31934043b2
Rain <Valehtelit@mail.ru>
2023-01-29 00:05:04 +1000

    # Настроил DTO

--- src/Application/Models/Search.php
+++ src/Application/Models/Search.php
@@ -25,27 +25,11 @@ class Search extends BaseDTO
     private function make(string $json) : void {
         $data = json_decode($json);
         $searchType = SearchType::from($data->searchType);
-        $data = (array) $data;
-        unset($data['searchType']);
-        $this->init($data);
+        $array = (array) $data;
+        unset($array['searchType']);
+        $this->init($array);
         $this->searchType = $searchType;
     }
-
-    /*private function init(string $json){
-        //Значит так. Убираем через unset трудные поля
-        //Далее прогоняем через init наследуемый все свойства
-        //Потом напрямую устанавливаем сложные поля например так: $this->searchType = SearchType::from($DTO->searchType);
-        $DTO = json_decode($json);
-        $this->searchType = SearchType::from($DTO->searchType);
-        $this->searchString = $DTO->searchString ?? null;
-        $this->startDate = $DTO->startDate ?? null;
-        $this->endDate = $DTO->endDate ?? null;
-        $this->employeeId = $DTO->employeeId ?? null;
-        $this->employeeTypeId = $DTO->employeeTypeId ?? null;
-        $this->counterpartyId = $DTO->counterpartyId ?? null;
-        $this->counterpartyTypeId = $DTO->counterpartyTypeId ?? null;
-        $this->correspondenceTypeId = $DTO->correspondenceTypeId ?? null;
-    }*/
 
     public function __get(string $name) : string|SearchType|null {
         return $this->$name;
