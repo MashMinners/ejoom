3c5e890a540520b100f9749e55a3ece4c0ac0e67
Rain <Valehtelit@mail.ru>
2023-02-14 18:41:25 +1000

    # API. Привел к нормальному виду маршруты

--- configs/routes.php
+++ configs/routes.php
@@ -5,10 +5,10 @@ $this->post('/', '\Application\Controllers\EJournalController::add');
 $this->put('/', '\Application\Controllers\EJournalController::save');
 $this->delete('/', '\Application\Controllers\EJournalController::remove');
 //Сотрудники
-$this->get('/employee', '\Application\Controllers\EmployeesController::get');
-$this->post('/employee', '\Application\Controllers\EmployeesController::add');
-$this->put('/employee', '\Application\Controllers\EmployeesController::save');
-$this->delete('/employee', '\Application\Controllers\EmployeesController::remove');
+$this->get('/employees', '\Application\Controllers\EmployeesController::get');
+$this->post('/employees', '\Application\Controllers\EmployeesController::add');
+$this->put('/employees', '\Application\Controllers\EmployeesController::save');
+$this->delete('/employees', '\Application\Controllers\EmployeesController::remove');
 //Контрагенты
 $this->get('/counterparties', '\Application\Controllers\CounterpartiesController::get');
 $this->post('/counterparties', '\Application\Controllers\CounterpartiesController::add');
