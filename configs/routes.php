b174fbe7fa2ee8d060ee5365cbf0fc923cc3b5c0
Rain <Valehtelit@mail.ru>
2023-02-20 20:31:50 +1000

    # API. Поправил маршруты

--- configs/routes.php
+++ configs/routes.php
@@ -1,9 +1,9 @@
 <?php
 //Журнал
-$this->get('/', '\Application\Controllers\EJournalController::get');
-$this->post('/', '\Application\Controllers\EJournalController::add');
-$this->put('/', '\Application\Controllers\EJournalController::save');
-$this->delete('/', '\Application\Controllers\EJournalController::remove');
+$this->get('/records', '\Application\Controllers\EJournalController::get');
+$this->post('/records', '\Application\Controllers\EJournalController::add');
+$this->put('/records', '\Application\Controllers\EJournalController::save');
+$this->delete('/records', '\Application\Controllers\EJournalController::remove');
 //Сотрудники
 $this->get('/employees', '\Application\Controllers\EmployeesController::get');
 $this->post('/employees', '\Application\Controllers\EmployeesController::add');
